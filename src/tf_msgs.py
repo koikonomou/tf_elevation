66aabc2d04286c6c5061681f27a3e5075b5cf5fb
cathrine.m.oik@gmail.com <cathrine.m.oik@gmail>
2017-10-18 16:05:33 +0300

    fixed tf

--- src/tf_msgs.py
+++ src/tf_msgs.py
@@ -4,66 +4,73 @@ import math
 import tf
 import numpy 
 from grid_map_msgs.msg import GridMap
-from tf2_msgs.msg import TFMessage
+from tf.msg import tfMessage
 
+tf_broadcaster = None
 
 def init(msg):
-    global tf_broadcaster, pub_elevation
+    global tf_broadcaster
     global height, position_x, position_y
 
 
     height=0
     position_x=0
     position_y=0
-    tf_broadcaster=None
-#    b=False
+    b=False
+    index=0
 
-    msg_ = msg
-    msg_.info.header.frame_id = "/camera_link"
-    msg_.info.header.stamp = rospy.Time.now()
-    msg_.info.resolution=0.06
-    msg_.outer_start_index=0
-    msg_.inner_start_index=0
-    msg_.info.pose.position.x=0.0
-    msg_.info.pose.position.y=0.0
-    msg_.info.pose.position.z=0.0
+    col_sz=msg.data[0].layout.dim[0].size
+    row_sz=msg.data[0].layout.dim[1].size
+    res = msg.info.resolution
+    p_x = msg.info.pose.position.x
+    p_y = msg.info.pose.position.y
+
+
 #    msg_.info.pose.orientation.x=0
 #    msg_.info.pose.orientation.y=0
 #    msg_.info.pose.orientation.z=0
-#    msg_.info.pose.orientation.w=1.0
+#    msg_.info.pose.orientation.w=1.
 
-    print len(msg_.data)
-    for i in range (0,numpy.ma.size(msg_.data)):
-        for j in range (0,numpy.ma.size(msg_.data[i].data)):
-            if numpy.isnan(msg_.data[i].data[j])==False :
-#                if (b==False):
-                height = msg_.data[i].data[j] #- 3.0 * 2.486
+    i=0
+    for k in range (len(msg.layers)):
+        if msg.layers[k]=="elevation" :
+            i = k
+            break
+    for j in range (0,numpy.ma.size(msg.data[i].data)):
+#            print len(msg_.data)
+#            print numpy.ma.size(msg_.data)
+#            print numpy.ma.size(msg_.data[i].data)
+#            print len(msg_.data[i].data)
+
+        if (numpy.isnan(msg.data[i].data[j])==False) and (numpy.isinf(msg.data[i].data[j])==False):
+#               if (b==False):
+#                height = msg_.data[i].data[j] #- 3.0 * 2.486
 #                    b=True
-            if (msg_.data[i].data[j] > height):
-                height = msg_.data[i].data[j]
-#                    if (msg_.data[i].data[j] == height):
-            position_x = j / 72.0 * 0.06
-            position_y = j % 72.0 * 0.06
-
-
-    tf_broadcaster = tf.TransformBroadcaster()
+            if (msg.data[i].data[j] > height):
+                height = msg.data[i].data[j]
+                index = j
+#        if (msg_.data[i].data[j] == height):
+    position_y = ((row_sz / 2) * res) - (int(index / row_sz) * res) + p_y
+    position_x = ((col_sz / 2) * res) - (int(index % col_sz) * res) + p_x
     tf_broadcaster.sendTransform(
-        (position_x / 3.24, position_y / 3.24, height),
+        (position_x , position_y , height),
         (0, 0, 0, 1.0),
         rospy.Time.now(),
         "elev_tf"+str(i),
-        "/camera_link")
+        "/base_link")
 
-    pub_elevation.publish(msg_)
+
 
     print (height)
+    print (index)
     print (position_x)
     print (position_y)
 
 
 if __name__=='__main__':
     rospy.init_node('tf_elevation')
-    pub_elevation=rospy.Publisher('/elev', GridMap, queue_size=1)
-    rospy.Subscriber('/elevation_mapping/elevation_map_raw', GridMap, init)
+    tf_broadcaster = tf.TransformBroadcaster()
+
+    rospy.Subscriber('/elevation_mapping/elevation_map', GridMap, init)
     while not rospy.is_shutdown():
         rospy.spin()
